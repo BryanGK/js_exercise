258e4ee695edf3a607d2f56a02330dfcc5dd12c3
BryanGK <bryangordonkrauss@gmail.com>
2021-01-07 18:51:45 -0800

    Another day of practice

---
+++ loop-exercise.js
@@ -0,0 +1,29 @@
+// let i = 0
+// while (i < 3) {
+//     alert(i);
+//     i++
+// }
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+// for (let i = 2; i < 10; i++) {
+//     if (i % 2  == 0) 
+
+//     alert(i);
+// }
+
+// let i = 0;
+
+// while (i < 3) {
+//     alert('number ' + i);
+//     i++
+// }
+
+for (i = 0; i <= 100;) {
+    i = prompt("Enter a number greater than 100", 0);
+    if (i > 100) {
+        break;
+    }
+}

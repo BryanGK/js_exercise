9028118ef85c7dbe4b5fb3a34afc548231d20b95
BryanGK <bryangordonkrauss@gmail.com>
2021-01-25 16:40:57 -0800

    #4 - Working thru JS exercises w3school.

--- practice2.js
+++ practice2.js
@@ -1,3 +1,28 @@
-document.getElementById("print").onclick = function () {
-    window.print();
+/* ====== #3 print window ====== */
+
+// document.getElementById("print").onclick = function () {
+//     window.print();
+// }
+
+
+/* ===== output current date ===== */
+
+
+let currentDay = new Date();
+
+let dayOfMonth = currentDay.getDate();
+let month = currentDay.getMonth() + 1;
+let year = currentDay.getFullYear();
+
+document.getElementById("display-date").onclick = function () {
+    if (month < 10)
+        month = "0" + month;
+    if (dayOfMonth < 10)
+        dayOfMonth = "0" + dayOfMonth
+    alert("Today's date is " + month + " / " + dayOfMonth + " / " + year);
 }
+
+console.log(currentDay);
+console.log(dayOfMonth);
+console.log(month);
+console.log(year);

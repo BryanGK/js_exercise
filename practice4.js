cca6c3cfa96c54d65133160e517d2bbd54d1b792
BryanGK <bryangordonkrauss@gmail.com>
2021-01-29 11:50:14 -0800

    More JS practice

---
+++ practice4.js
@@ -0,0 +1,56 @@
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("Truthy")
+// } else {
+//     console.log("Falsey")
+// }
+
+// if (0) {
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsey");
+// }
+
+// if (1) {
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsey");
+// }
+
+// if (" ") {                      // truthy
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsey");
+// }
+
+// const password = prompt("Enter password");
+
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid Password");
+// } else {
+//     console.log("Invalid Password");
+// }
+
+// const age = 10;
+
+// if ((age >= 0 && age <= 5) || age >= 65) {
+//     console.log("Free");
+// } else if (age > 5 && age <= 10) {
+//     console.log("$10")
+// } else if (age > 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("Invalid Age")
+// }
+
+// const firstName = prompt("Enter your first name");
+// if (!firstName) {
+//     firstName = prompt("Enter your first name")
+// }
+
+// const age = 45;
+
+// if (!(age >= 0 && age <= 5 || age >= 65)) {
+//     console.log("Not baby or senior");
+// }
